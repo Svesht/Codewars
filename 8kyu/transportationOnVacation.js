dd062df190941795b78bf472403ac2038b277c81
Arthur Panteleev <arthur.panteleev@gmail.com>
2022-09-25 18:43:32 +0200

    Transportation on Vacation

---
+++ 8kyu/transportationOnVacation.js
@@ -0,0 +1,8 @@
+function rentalCarCost(d) {
+    const baseCost = 40 * d
+    if (d > 6) return baseCost - 50;
+    if (d > 2) return baseCost - 20;
+    return baseCost;
+}
+
+//Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
