2d71ec554dfc6b3857cbfef307105cbe13de72e8
Arthur Panteleev <arthur.panteleev@gmail.com>
2022-11-01 11:57:17 +0200

    Grasshopper - Messi Goals

---
+++ 8kyu/grasshopperMessiGoals.js
@@ -0,0 +1,7 @@
+// Original solution
+
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
