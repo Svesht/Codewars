369fc698adb71e4dbd4d449645666ca6f8f24dc7
Arthur Panteleev <arthur.panteleev@gmail.com>
2022-11-18 12:37:16 +0200

    I love you a little, a lot, passionately... not at all.

---
+++ 8kyu/iLoveYouALittleALotPassionatelyNotAtAll.js
@@ -0,0 +1,6 @@
+//Original solution
+
+function howMuchILoveYou(nbPetals) {
+    const arr = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
+    return arr[(nbPetals - 1) % arr.length];
+}
