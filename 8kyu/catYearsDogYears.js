21637b41e9af66956811359dae678ecc35cebe15
Arthur Panteleev <arthur.panteleev@gmail.com>
2022-12-19 09:53:15 +0100

    Cat Years, Dog Years

--- 8kyu/catYearsDogYears.js
+++ 8kyu/catYearsDogYears.js
@@ -1,16 +1,24 @@
 //Original solution
 
-var humanYearsCatYearsDogYears = function(humanYears) {
-    let catYears = 15;
-    let dogYears = 15;
-    for (i = 1; i < humanYears; i++) {
-        if (i === 1) {
-            catYears += 9;
-            dogYears += 9;
-        } else {
-            catYears += 4;
-            dogYears += 5;
-        }
-    }
-  return [humanYears, catYears, dogYears];
+// var humanYearsCatYearsDogYears = function(humanYears) {
+//     let catYears = 15;
+//     let dogYears = 15;
+//     for (i = 1; i < humanYears; i++) {
+//         if (i === 1) {
+//             catYears += 9;
+//             dogYears += 9;
+//         } else {
+//             catYears += 4;
+//             dogYears += 5;
+//         }
+//     }
+//   return [humanYears, catYears, dogYears];
+// }
+
+//Faster solution
+
+const humanYearsCatYearsDogYears = function(years) {
+    if (years === 1) return [1,15,15];
+    if (years === 2) return [2,24,24];
+    return [years, 24 + 4*(years - 2), 24 + 5*(years - 2)];
 }
