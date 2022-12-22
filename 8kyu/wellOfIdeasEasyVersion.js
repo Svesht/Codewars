ee797b8c772f7e9699d04dc38ef6fe54f97c1cf7
Arthur Panteleev <arthur.panteleev@gmail.com>
2022-12-22 11:50:18 +0100

    Well of Ideas - Easy Version

--- 8kyu/wellOfIdeasEasyVersion.js
+++ 8kyu/wellOfIdeasEasyVersion.js
@@ -9,3 +9,12 @@ function well(ideas) {
     if (good > 0) return "Publish!";
     return "Fail!";
 }
+
+//More compact solution using filter
+
+function well(x) {
+    const good = x.filter(idea => idea === 'good').length;
+    if (good > 2) return "I smell a series!";
+    if (good > 0) return "Publish!";
+    return "Fail!";
+}
