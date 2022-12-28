9139bfc94b4dc43aee1386166a382e700d0a5a40
Arthur Panteleev <arthur.panteleev@gmail.com>
2022-12-28 10:01:40 +0100

    Vowel Remover

--- 8kyu/vowelRemover.js
+++ 8kyu/vowelRemover.js
@@ -1,5 +1,11 @@
+//Original solution
+
 function shortcut (string) {
     const vowels = ['a','e','i','o','u'];
     const str = string.split('').filter(letter => !vowels.includes(letter));
     return str.join('');
 }
+
+//Solution with regex
+
+const shortcut = string => string.replace(/[aeiou]/g, '');
