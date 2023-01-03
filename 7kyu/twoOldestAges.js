75331b5229d2615593410f16eb5f827f780034ff
Arthur Panteleev <arthur.panteleev@gmail.com>
2023-01-03 13:16:42 +0100

    Two Oldest Ages

--- 7kyu/twoOldestAges.js
+++ 7kyu/twoOldestAges.js
@@ -1,7 +1,18 @@
+//Original solution
+
+// const twoOldestAges = ages => {
+//     const arr = [...ages];
+//     return arr
+//         .sort((a,z) => z - a)
+//         .slice(0, 2)
+//         .reverse();
+// }
+
+//Shorter version without reverse
+
 const twoOldestAges = ages => {
     const arr = [...ages];
     return arr
-        .sort((a,z) => z - a)
-        .slice(0, 2)
-        .reverse();
-  }
+        .sort((a, z) => a - z)
+        .slice(-2);
+}
