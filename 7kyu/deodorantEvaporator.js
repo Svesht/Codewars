e82065f3707e39932fb31fa94b91a8770eda9a0c
Arthur Panteleev <arthur.panteleev@gmail.com>
2023-01-28 12:54:11 +0100

    Deodorant Evaporator

--- 7kyu/deodorantEvaporator.js
+++ 7kyu/deodorantEvaporator.js
@@ -1,3 +1,5 @@
+//Original solution
+
 function evaporator(content, evap_per_day, threshold) {
     let day = 0;
     const minContent = content * (threshold / 100)
@@ -5,6 +7,19 @@ function evaporator(content, evap_per_day, threshold) {
     while (content > minContent) {
         day++;
         content -= content * (evap_per_day / 100);
+    }
+    return day;
+}
+
+//Cleaner version using percentages
+
+function evaporator(content, evap_per_day, threshold) {
+    let day = 0;
+    let gas = 100;
+    
+    while (gas > threshold) {
+        day++;
+        gas -=  gas * (evap_per_day / 100);
     }
     return day;
 }
