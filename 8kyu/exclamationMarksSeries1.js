da64dc7b975d429f15393e66f4c3c665f617ad34
Arthur Panteleev <arthur.panteleev@gmail.com>
2023-02-10 18:56:17 +0100

    Exclamation marks series #1: Remove an exclamation mark from the end of string

--- 8kyu/exclamationMarksSeries1.js
+++ 8kyu/exclamationMarksSeries1.js
@@ -1,5 +1,11 @@
+//Original solution
+
 function remove (string) {
     const lastIndex = string.length - 1;
     if (string[lastIndex] === '!') return string.slice(0, lastIndex);
     return string;
 }
+
+//Shorter using RegEx
+
+const remove = string => string.replace(/!$/, '');
