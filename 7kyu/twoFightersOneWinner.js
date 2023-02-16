9c8c2cde5b3de7806ea45a9dafa7b1f2ab3f539d
Arthur Panteleev <arthur.panteleev@gmail.com>
2023-02-16 10:18:06 +0100

    Two fighters, one winner

--- 7kyu/twoFightersOneWinner.js
+++ 7kyu/twoFightersOneWinner.js
@@ -7,3 +7,13 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
     if (turnsToWinFighter1 > turnsToWinFighter2) return fighter2.name;
     return (fighter1.name === firstAttacker) ? fighter1.name : fighter2.name;
 }
+
+//Cleanup at the end
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    const turnsToWinFighter1 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
+    const turnsToWinFighter2 = Math.ceil(fighter1.health / fighter2.damagePerAttack);
+    if (turnsToWinFighter2 > turnsToWinFighter1) return fighter1.name;
+    if (turnsToWinFighter1 > turnsToWinFighter2) return fighter2.name;
+    return firstAttacker;
+}
