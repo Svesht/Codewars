afbdd218429120b5c1be8600fbb87d367e09ab57
Arthur Panteleev <arthur.panteleev@gmail.com>
2023-02-16 10:12:03 +0100

    Two fighters, one winner

---
+++ 7kyu/twoFightersOneWinner.js
@@ -0,0 +1,9 @@
+//First solution
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    const turnsToWinFighter1 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
+    const turnsToWinFighter2 = Math.ceil(fighter1.health / fighter2.damagePerAttack);
+    if (turnsToWinFighter2 > turnsToWinFighter1) return fighter1.name;
+    if (turnsToWinFighter1 > turnsToWinFighter2) return fighter2.name;
+    return (fighter1.name === firstAttacker) ? fighter1.name : fighter2.name;
+}
